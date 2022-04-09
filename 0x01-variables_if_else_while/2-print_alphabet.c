d7fd28798495604802478794d873c32d2168f386
BluvD143 <a.bello143@yahoo.com>
2022-04-09 06:44:57 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,13 @@
  *
  *  Description: Print the lowercase alphabet
  *  Return: 0
- *  */
+ */
 int main(void)
 {
 int c;
 
 for (c = 0; c < 26; ++c)
-putchar('a' +c);
+putchar('a' + c);
 
 putchar('\n');
 
