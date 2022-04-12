68788297cb2777afa88e5d0611912a0088be9f83
BluvD143 <a.bello143@yahoo.com>
2022-04-12 08:20:30 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+int print_alphabet(void)
 {
 char letter;
 
@@ -12,5 +12,4 @@ for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 
 _putchar('\n');
-return (0);
 }
