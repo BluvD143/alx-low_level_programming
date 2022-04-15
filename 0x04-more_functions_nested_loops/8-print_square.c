e164db16916988c8759d41ab481b957e7f62e4ba
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-04-15 13:08:48 +0100

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,28 +6,25 @@
   *
   * Return: empty
   */
+
 void print_square(int size)
 {
-	int i, k;
+	int x, y;
 
-	if (size > 0)
+	if (size <= 0)
 	{
-		for (i = 0; i < size; i++)
+		_putchar('\n');
+	}
+	else
 	{
-		for (k = 0; k < size; k++)
+		for (x = 0; x < size; x++)
 		{
-				_putchar(char 35);
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
 			}
 
 			_putchar('\n');
-            
-           }
-           
+		}
 	}
-    
-    else
-    {
-    _putchar('\n');
-    }
-    
 }
