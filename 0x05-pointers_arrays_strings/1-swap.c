2201ee4cc1aa44df18d19a63b89cedf9e0c60156
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-04-21 14:58:51 +0100

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,13 +2,17 @@
 
 /**
  * swap_int - swaps the values of two integers
+ * using two input parameters
  * @u: pointer 1
  * @v: pointer 2
+ *
  * Return: void
  */
 
 void swap_int(int *u, int *v)
 {
 	int y = *u;
+	
 	*u = *v;
 	*v = y;
+}
