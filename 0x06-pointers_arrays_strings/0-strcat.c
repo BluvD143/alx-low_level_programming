cfdf1cf5d434ef0aa18f82ae876656701cb7b02d
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-04-23 09:45:08 +0100

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate two strings,
- * @dest: destination.
- * @src: source.
- * Return: the pointer to dest
+ *_strcat - concatinate 2 strings.
+ *@dest: destination.
+ *@src: source.
+ *Return: string.
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int count = 0; j = 0;
+	int i = 0, j = 0;
 
-	while (*(dest + count) != '\0')
-	{
-		count++;
-	}
+	while (dest[i] != '\0')
+		i++;
 
-	while (j >= 0)
+	while (src[j] != '\0')
 	{
-		*(dest + count) = *(src + j);
-		if (*(src + j) == '\0')
-		break;
-		count++;
+		dest[i] = src[j];
+		i++;
 		j++;
 	}
+
+	dest[i] = '\0';
+
 	return (dest);
 }
