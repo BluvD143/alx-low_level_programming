07601c95923c356eeb04fecad320ab6628cdaeb2
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-04-26 15:25:07 +0100

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * _strstr - returns pointer to first char of matching substring
+ * _strstr - this returns pointer to first char of matching substring
  * @haystack: string to find substring in
  * @needle: substring to find match of
+ *
  * Return: pointer to first char of matching substring
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int k;
+	int m;
 
 	while (*haystack != '\0')
 	{
-		k = 0;
+		m = 0;
 		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
-			haystack++, needle++, k++;
+			haystack++, needle++, m++;
 		if (*needle == '\0')
-			return (haystack - k);
-		haystack -= (k - 1), needle -= k;
+			return (haystack - m);
+		haystack -= (m - 1), needle -= m;
 	}
 	return ('\0');
 }
