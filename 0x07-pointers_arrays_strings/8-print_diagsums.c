68965baf58725cb5776b773912b8f6a0cd9581f9
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-04-26 15:53:52 +0100

    Task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints sum of two diagnols of a square matrix
+ * @a: input square array
+ * @size: size of the dimension of the array
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int m, n, sum1 = 0, sum2 = 0;
+
+	for (n = 0, m = size - 1; n < (size * size); n += size + 1, m += size - 1)
+		sum1 += a[n], sum2 += a[m];
+	printf("%d, %d\n", sum1, sum2);
+}
