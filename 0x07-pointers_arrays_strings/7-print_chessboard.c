213e1656561539eadf2bef41c7bde5fe01d23a59
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-04-26 15:30:59 +0100

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - this function prints a chessboard
+ * @a: an array input to print
+ *
+ * Return: Nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int k = 0, m;
+
+	for (; k < 8; k++)
+	{
+		for (m = 0; m < 8; m++)
+			_putchar(a[k][m]);
+		_putchar('\n');
+	}
+}
