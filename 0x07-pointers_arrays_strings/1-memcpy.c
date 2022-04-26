53248244c839dbde50b58c2552743a9d94306e14
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-04-26 14:10:42 +0100

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,12 +11,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int j = 0;
 
-	while (i < n)
+	while (j < n)
 	{
-		*(dest + i) = *(src + i);
-		i++;
+		*(dest + j) = *(src + j);
+		j++;
 	}
 	return (dest);
 }
