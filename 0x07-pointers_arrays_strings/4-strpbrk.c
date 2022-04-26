33d466c1a5ab2c86840308eeec5766d779436617
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-04-26 15:00:49 +0100

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strpbrk - this search for a string for any of the set of bytes
+ * @s: input string to search for matching char
+ * @accept: characters that could be matched
+ *
+ * Return: pointer to matching char
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int k, b;
+
+	for (k = 0; s[k] != '\0'; k++)
+		for (b = 0; accept[b] != '\0'; b++)
+			if (s[k] == accept[b])
+				goto exit;
+exit: return (s[k] != '\0' ? s + k : '\0');
+}
