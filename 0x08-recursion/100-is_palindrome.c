edaf6bdbe0bfbe611313663e1a99f6c25a1398c5
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-04-29 03:07:57 +0100

    Task 7 - Returns 1 if a string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,23 +5,16 @@
  * @a: string
  * @l: integer to count length
  *
- * Return: On success 1.
+ * Return: the length of a string
  */
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        z#include "main.h"
 
-/**
- * palind2 - obtains length of a
- * @a: string
- * @l: integer to count length
- *
- * Return: On success 1.
- */
 int palind2(char *a, int l)
 {
 	if (*a == 0)
 		return (l - 1);
 	return (palind2(a + 1, l + 1));
 }
+
 /**
  * palind3 - compares string vs string reverse
  * @a: string
@@ -38,47 +31,14 @@ int palind3(char *a, int l)
 		return (1);
 	return (palind3(a + 1, l - 2));
 }
+
 /**
  * is_palindrome - checks if a string is a palindrome
  * @s: string to evaluate
  *
- * Return: On success 1.
- */
-int is_palindrome(char *s)
-{
-	int l;
-
-	l = palind2(s, 0);
-	return (palind3(s, l));
-}
-int palind2(char *a, int l)
-{
-	if (*a == 0)
-		return (l - 1);
-	return (palind2(a + 1, l + 1));
-}
-/**
- * palind3 - compares string vs string reverse
- * @a: string
- * @l: length
- *
- * Return: On success 1.
+ * Return: 1 if s is a palindrome, 0 if not.
  */
 
-int palind3(char *a, int l)
-{
-	if (*a != *(a + l))
-		return (0);
-	else if (*a == 0)
-		return (1);
-	return (palind3(a + 1, l - 2));
-}
-/**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to evaluate
- *
- * Return: On success 1.
- */
 int is_palindrome(char *s)
 {
 	int l;
