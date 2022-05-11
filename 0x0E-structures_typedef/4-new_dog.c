4d8c174cf9ae2938f679ef9f670b9f53188e49b3
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-05-11 18:06:05 +0100

    Task 4 - A function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,23 +1,26 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
  * new_dog - is a function that creates a new dog
  * @name: is a pointer to name
  * @age: is a pointer to age
  * @owner: is a pointer owner
+ *
  * Return: pointer to new_dog
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 
-	int i = 0, j = 0, m;
+	int x = 0, y = 0, p;
 	dog_t *dogo;
 
-	while (name[i] != '\0')
-		i++;
-	while (owner[j] != '\0')
-		j++;
+	while (name[x] != '\0')
+		x++;
+	while (owner[y] != '\0')
+		y++;
 
 	dogo = malloc(sizeof(dog_t));
 	if (dogo == NULL)
@@ -25,17 +28,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dogo);
 		return (NULL);
 	}
-	dogo->name = malloc(i * sizeof(dogo->name));
+	dogo->name = malloc(x * sizeof(dogo->name));
 	if (dogo->name == NULL)
 	{
 		free(dogo->name);
 		free(dogo);
 		return (NULL);
 	}
-	for (m = 0; m <= i; m++)
-		dogo->name[m] = name[m];
+	for (p = 0; p <= x; p++)
+		dogo->name[p] = name[p];
 	dogo->age = age;
-	dogo->owner = malloc(j * sizeof(dogo->owner));
+	dogo->owner = malloc(y * sizeof(dogo->owner));
 	if (dogo->owner == NULL)
 	{
 		free(dogo->owner);
@@ -43,7 +46,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dogo);
 		return (NULL);
 	}
-	for (m = 0; m <= j; m++)
-		dogo->owner[m] = owner[m];
+	for (p = 0; p <= y; p++)
+		dogo->owner[p] = owner[p];
 	return (dogo);
 }
