7a85602ae9d7e897b1625e38f3a82c7ab63b2fc3
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-05-24 22:07:30 +0100

    Task 1 - Returns the number of elements in L-list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - returns number of elements in a list_t list
+ * list_len - this returns number of elements in a list_t list
  * @h: the list to be used
  *
  * Return: the number of elements in list
@@ -15,7 +15,7 @@ size_t list_len(const list_t *h)
 	while (l != NULL)
 	{
 		l = l->next;
-    rice++;
-	}
-	return (rice);
+      rice++;
+  }
+  return (rice);
 }
