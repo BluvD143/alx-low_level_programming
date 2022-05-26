6fd157b599da3752022566981b4d8d47d95ddeef
BluvD143 <a.bello143@yahoo.com>
2022-05-26 11:30:03 -0700

    Task 0 - Prints all the elements of a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - this prints all the elements of a listint_t list
+ * @h: a pointer to the lis
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t rice = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		rice++;
+		h = h->next;
+	}
+	return (rice);
+}
