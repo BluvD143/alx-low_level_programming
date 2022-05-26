e319b7e11a9f518c5ba15f76b64d0cb012d321e5
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-05-26 21:12:31 +0100

    Task 4 - frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - this frees the memory of a list
+ * @head: is the pointer to the start of the list
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+  listint_t *rice;
+
+	while (head)
+	{
+		rice = head->next;
+		free(head);
+		head = rice;
+	}
+}
