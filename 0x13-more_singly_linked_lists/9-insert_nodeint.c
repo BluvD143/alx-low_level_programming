654aade433c638f37f7d257938fa1211401c448f
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-05-27 05:18:45 +0100

    Task 9 - inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,16 +1,13 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - Inserts a new node to a listint_t
- *                           list at a given position.
- * @head: A pointer to the address of the
- *        head of the listint_t list.
+ * insert_nodeint_at_index - Inserts a new node to a list at a given position.
+ * @head: A pointer to the address of the head of the list.
  * @idx: The index of the listint_t list where the new
  *       node should be added - indices start at 0.
  * @n: The integer for the new node to contain.
  *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
+ * Return: NULL if function fails Otherwise - the address of the new node.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
